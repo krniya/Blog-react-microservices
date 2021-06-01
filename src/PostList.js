f1408721d77b4eec211f2e99b78543a2c4244fc0
Nitish Kumar <krnitish@live.in>
2021-06-01 23:45:18 +0530

    Render Post configured

--- src/PostList.js
+++ src/PostList.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function PostList(props) {
     const [posts, setPost] = useState([])
+    const fetchPost = async () => {
+        const res = await axios.get('https://localhost:4000/posts')
+        setPost(res.data)
+    }
+    useEffect(() => {
+        fetchPost()
+    }, [])
 
+    const renderPost = Object.values(posts).map(post => {
+        return <div
+            className="card"
+            style={{ width: '30%', marginBottom: '20px' }}
+            key={post.id}
+        >
+            <div className="card-body">
+                <h3>{post.title}</h3>
+            </div>
+        </div>
+    })
 
-    return <div />
+    return <div className="d-flex flex-row  flex-wrap justify-content-between">
+        {renderPost}
+    </div>
 
 }
